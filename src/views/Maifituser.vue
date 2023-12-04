bd34e57c82c67ff95c3d4caae63b373f0c165fdb
wldydEnajd <anwhrjs815@naver.com>
2023-12-05 00:02:25 +0900

    make a base for User

---
+++ src/views/Maifituser.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>체형예측에 사용할 본인의 사진을 올려주세요. (jpg only)</p>
+        <input type="file" accept="image/jpeg">
+        <button style="float: right;">다음</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MaifitUser',
+}
+</script>
