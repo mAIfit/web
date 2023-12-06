b4a97c457068931d26232920421d91772e75dde5
Jaewoong Jung <jaewoong_jung@naver.com>
2023-12-06 13:23:55 +0900

    feat: display product info passed by API

---
+++ src/views/MaifitProductInfo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="image-container">
+            <img :src="image_path" alt="Product Image" />
+        </div>
+        <div class="info-container">
+            <h2>{{ brand }} {{ name }}</h2>
+        </div>
+        <div class="button-container">
+            <button @click="goToPrev">Prev</button>
+            <button @click="goToNext">Next</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MaifitProductInfo",
+    data() {
+        return {
+            image_path: "",
+            brand: "",
+            name: ""
+        };
+    },
+    methods: {
+        goToPrev() {
+            this.$router.push("/");
+        },
+        goToNext() {
+            this.$router.push("/maifit/user");
+        }
+    },
+    created() {
+        this.image_path = this.$route.params.image_path;
+        this.brand = this.$route.params.brand;
+        this.name = this.$route.params.name;
+    }
+};
+</script>
+
+<style scoped>
+.image-container {
+    width: 50%;
+    float: left;
+}
+
+.info-container {
+    width: 50%;
+    float: left;
+}
+
+.button-container {
+    text-align: right;
+    margin-top: 20px;
+}
+</style>
