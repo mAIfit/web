30db5fc69e4fa9f0f02e0cc8f4482587a23d501f
Jaewoong Jung <jaewoong_jung@naver.com>
2023-12-06 20:46:43 +0900

    BE configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://port-0-maifit-server-eu1k2lllf479w5.sel3.cloudtype.app/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 })
