47cd6f410bd7715c7f8900bbf1b55d4da7e3f8b8
wldydEnajd <anwhrjs815@naver.com>
2023-12-06 15:04:50 +0900

    make a little of Loading.vue

---
+++ src/views/MaifitLoading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="centered-text">Searching from Internet...</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  height: 70vh;
+}
+
+.centered-text {
+  text-align: center;
+  font-size: 45px;
+}
+</style>
