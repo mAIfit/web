fbb54b22972b45a29accca2863c9337e6dec25cc
wldydEnajd <anwhrjs815@naver.com>
2023-12-06 19:16:53 +0900

    add sex,height

--- src/views/MaifitUser.vue
+++ src/views/MaifitUser.vue
@@ -1,14 +1,17 @@
+
 <template>
   <div class="container">
     <img src="@/assets/logo.png" alt="Logo" class="logo" />
     <h1 class="title">체형예측에 사용할 본인의 사진을 올려주세요. (jpg only)</h1>
-    <div class="big-box">
       <div class="image-upload">
         <input type="file" accept="image/jpeg" @change="handleImageUpload" />
         <div v-if="selectedImage" class="preview">
           <img :src="selectedImage" alt="Selected Image" />
         </div>
       </div>
+    <div class="input-fields">
+      <input type="string" v-model="sex" placeholder="성별" />
+      <input type="number" v-model="height" placeholder="키(cm)" />
     </div>
     <button class="next-button" @click="uploadImage">다음</button>
   </div>
@@ -18,7 +21,9 @@
 export default {
   data() {
     return {
-      selectedImage: null
+      selectedImage: null,
+      sex: null,
+      height: null
     };
   },
   methods: {
@@ -31,11 +36,13 @@ export default {
       reader.readAsDataURL(file);
     },
     uploadImage() {
-      // Convert the selected image to FormData
+      // Convert the selected image, sex and height to FormData
       const formData = new FormData();
+      formData.append('sex', this.sex);
+      formData.append('height', this.height);
       formData.append('image', this.selectedImage);
 
-      // Send the image to the server using POST request
+      // Send the data to the server using POST request
       fetch('/api/upload', {
         method: 'POST',
         body: formData
@@ -75,30 +82,47 @@ export default {
   margin-top: 20px;
 }
 
-.big-box {
-  width: 500px;
-  height: 200px;
-  border: 1px solid black;
-  border-radius: 10px;
-  margin-bottom: 20px;
-}
-
 .image-upload {
-  display: flex;
   flex-direction: column;
-  align-items: center;
-  margin-top: 80px;
-  margin-left: 100px;
-  margin-bottom: 100px;
+  margin-top: 30px;
+  margin-bottom: 200px;
+  text-align: center;
 }
 
 .preview {
-  margin-top: 20px;
+  /* 크기를 일치시킴 */
+  width: 50%;
+  height: 50%;
+  overflow: hidden;
+  margin: auto;
+  position: relative; /* 추가 */
+  z-index: 1;
+}
+
+.preview img {
+  max-width: 100%;
+  max-height: 100%;
+  margin-top: 50px;
+}
+
+.input-fields {
+  display: flex;
+  flex-direction: column; /* 위아래로 정렬하기 위해 flex-direction: column; 추가 */
+  align-items: center; /* 수직 정렬을 위해 align-items 추가 */
+  margin-top : 580px;
+  position: absolute;
+}
+
+.input-fields input {
+  padding: 10px 20px;
+  margin-right: 20px;
 }
 
 .next-button {
   padding: 10px 20px;
   font-size: 16px;
-  margin-left: 500px;
+  margin-left: 700px;
+  margin-top: 550px;
+  position: absolute;
 }
 </style>
